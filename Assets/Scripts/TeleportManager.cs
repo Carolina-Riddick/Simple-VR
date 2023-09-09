05e131639eeff2e63a9462b95735b6e1ea8f5110
Carolina Riddick <carolinariddick@gmail.com>
2023-09-09 15:07:36 +1000

    simple VR movement and features

---
+++ Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class TeleportManager : MonoBehaviour
+{
+
+    public InputActionReference teleportActionReference;
+
+    public UnityEvent onTeleportActive;
+    public UnityEvent onTeleportCancel;
+
+
+    private void Start()
+    {
+        teleportActionReference.action.performed += teleportActivate;
+
+        teleportActionReference.action.canceled -= teleportCancell;
+    }
+
+    // Activo el teleport Activo
+    private void teleportActivate(InputAction.CallbackContext context) => onTeleportActive.Invoke();
+
+
+
+    // Activo el teleport para cancelarlo
+    void DelayedTeleportCancel()
+    {
+        onTeleportCancel.Invoke();
+    }
+
+
+    // Invoco al metodo con una tardanza de 0.01 seg
+    private void teleportCancell(InputAction.CallbackContext context) {
+
+        Invoke("DelayedTeleportCancel", 0.1f);
+    }
+
+
+        }
+    
+   
+    
